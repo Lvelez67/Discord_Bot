430695a69d95453e4dc890186fe6929ed250a2c9
lvelez98 <lvelez679@gmail.com>
2023-12-26 12:54:33 -0500

    Continued tut series

---
+++ context_menu_commands.py
@@ -0,0 +1,39 @@
+import settings
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+logger = settings.logging.getLogger("bot")
+
+
+def run():
+  intents = discord.Intents.default()
+  intents.message_content = True
+  intents.members = True
+  
+  bot = commands.Bot(command_prefix="~", intents=intents)
+  
+  @bot.event
+  async def on_ready():
+    logger.info(f"User: {bot.user} (ID: {bot.user.id})")
+    
+    bot.tree.copy_global_to(guild=settings.GUILDS_ID)
+    await bot.tree.sync(guild=settings.GUILDS_ID)
+    
+  @bot.tree.context_menu(name="Show join date")
+  async def get_joined_date(interaction: discord.Interaction, member: discord.Member):
+    await interaction.response.send_message(f"Member joined: {discord.utils.format_dt(member.joined_at)} ", ephemeral=True)
+      
+  @bot.tree.context_menu(name="Report Message")
+  async def report_message(interaction: discord.Interaction, message: discord.Message):
+    await interaction.response.send_message(f"Message Reported ", ephemeral=True)
+  #Purposefully gives an error (down) 
+  @bot.tree.context_menu(name="Report Message")
+  async def report_message(interaction: discord.Interaction, channel: discord.VoiceChannel):
+    await interaction.response.send_message(f"Message Reported ", ephemeral=True)
+ 
+    
+  bot.run(settings.DISCORD_API_SECRET, root_logger=True)
+  
+if __name__ == "__main__":
+  run()
