777d3977c332dc4afb659bb74af3c2da10ad18bf
lvelez98 <lvelez679@gmail.com>
2023-12-26 01:01:22 -0500

    Testing slash commands

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ import os
 import logging
 from logging.config import dictConfig
 from dotenv import load_dotenv
+import discord
 
 load_dotenv()
 
@@ -11,8 +12,9 @@ DISCORD_API_SECRET = os.getenv("DISCORD_API_TOKEN")
 BASE_DIR = pathlib.Path(__file__).parent
 
 CMDS_DIR = BASE_DIR / "cmds"
-
 COGS_DIR = BASE_DIR / "cogs"
+
+GUILDS_ID = discord.Object(id=int(os.getenv("GUILD")))
 
 LOGGING_CONFIG = {
     "version":1,
